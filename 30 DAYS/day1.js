6679dfa61bafa89ca84b6df6392a59517dc65be7
vishu118 <114770536+vishu118@users.noreply.github.com>
2023-04-27 09:06:35 +0530

    day2

--- 30 DAYS/day1.js
+++ 30 DAYS/day1.js
@@ -12,3 +12,21 @@ var b = true;
 
 var c = null;
 
+console.log(typeof 2)
+console.log(typeof true)
+console.log(typeof null)
+console.log(typeof undefined)
+
+
+b = 33;
+
+console.log(b)
+
+let num = 34;
+
+
+let firstName ="Vishal"
+let lastName ="Kumar"
+
+let fullName = `${firstName} ${lastName}`
+console.log(fullName)
