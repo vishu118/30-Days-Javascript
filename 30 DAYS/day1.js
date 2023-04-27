f823214925345cea9b38ad028c09b16d6746921d
vishu118 <114770536+vishu118@users.noreply.github.com>
2023-04-27 08:59:40 +0530

    basic practice

---
+++ 30 DAYS/day1.js
@@ -0,0 +1,14 @@
+// Comment can make code readable
+
+// Welcome To 30 Days JavaScript Challenge
+
+
+/* comment can make your code readable
+and molre precise*/
+
+var str = "30 Days Javascript challange"
+
+var b = true;
+
+var c = null;
+
---
+++ app.js
@@ -0,0 +1,3 @@
+let arr = [1,3,5,7,8]
+let map = new Map([arr,arr])
+console.log(map)
